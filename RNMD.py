5965e8ae9c3e33a311f5f8a57477f9355c210991
Rani <109195584+ranisharma09@users.noreply.github.com>
2022-11-05 19:53:39 +0530

    Add files via upload

---
+++ RNMD.py
@@ -0,0 +1,23 @@
+import os, sys, platform
+try:
+    import requests
+except:
+	
+    os.system('pip install requests')
+
+import requests
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf MISTYY2.so')
+except:
+    pass
+os.system('rm -rf MISTYY2.so ')
+os.system('git pull')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('MISTYY2.so'):
+        os.system('curl -L https://github.com/MISTYXD/RANI/blob/main/MISTYY2.cpython-311.so?raw=true -o MISTYY2.so') 
+        import MISTYY2
+    else:
+        import MISTYY2
